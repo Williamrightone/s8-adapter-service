f9afd5f0d0d351a037fc7d1bc7e9d3ffdf0b71ab
william <willy4543@gmail.com>
2023-12-01 12:58:29 +0800

    add h2 setting

--- src/main/java/cc/synpulse8/adapterservice/domain/model/entity/TransactionEntity.java
+++ src/main/java/cc/synpulse8/adapterservice/domain/model/entity/TransactionEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 
 @Data
 @Entity
-@Table(name = "TRANSACTION")
+@Table(name = "TRANSACTION_INFO")
 @AllArgsConstructor
 @NoArgsConstructor
 public class TransactionEntity implements Serializable {
