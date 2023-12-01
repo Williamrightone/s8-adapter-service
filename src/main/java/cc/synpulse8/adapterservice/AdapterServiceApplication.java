093729223af5cfff1acfe1b4d34ed472826eb870
william <willy4543@gmail.com>
2023-12-01 11:34:12 +0800

    init project

---
+++ src/main/java/cc/synpulse8/adapterservice/AdapterServiceApplication.java
@@ -0,0 +1,13 @@
+package cc.synpulse8.adapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdapterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/synpulse8/adapterservice/AdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.synpulse8.adapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
